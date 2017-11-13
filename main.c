5ace46077995872a3a0298f341f6adf0a952187b
alessandroiori <alessandro.iori92@gmail.com>
2017-11-14 00:00:54 +0100

    show two concurrent thread running without interference

--- main.c
+++ main.c
@@ -8,12 +8,23 @@
  * gcc -lpthread main.c -o test.exe
  */
 
-void* thread_function(void* args)
+void* thread_function1(void* args)
 {
     int i;
     for (i = 0; i < 20; ++i)
     {
-        printf("%d - Thread say hi!\t\n", i);
+        printf("1.%d - Thread say hi!\t\n", i);
+        sleep(1);
+    }
+    return NULL;
+}
+
+void* thread_function2(void* args)
+{
+    int i;
+    for (i = 0; i < 20; ++i)
+    {
+        printf("2.%d - Thread say hi!\t\n", i);
         sleep(1);
     }
     return NULL;
@@ -23,19 +34,32 @@ int main()
 {
     //printf("Hello, World!\n");
     //return 0;
-    pthread_t mythread;
+    pthread_t mythread1;
+    pthread_t mythread2;
 
-    if(pthread_create(&mythread, NULL, thread_function, NULL))
+    if(pthread_create(&mythread1, NULL, thread_function1, NULL))
     {
-        printf("error creating thread\t\n");
+        printf("error creating thread 1\t\n");
+        abort();
+    }
+
+    if(pthread_create(&mythread2, NULL, thread_function2, NULL))
+    {
+        printf("error creating thread 2\t\n");
         abort();
     }
 
     printf("before join\t\n");
 
-    if(pthread_join(mythread, NULL))
+    if(pthread_join(mythread1, NULL))
     {
-        printf("error join thread\t\n");
+        printf("error join thread 1\t\n");
+        abort();
+    }
+
+    if(pthread_join(mythread2, NULL))
+    {
+        printf("error join thread 2\t\n");
         abort();
     }
     exit(0);
