f23f14bf6ca4ba34ce7cb533c36df6110576559e
alessandroiori <alessandro.iori92@gmail.com>
2017-11-18 16:29:42 +0100

    added exec_fork3.c

---
+++ examples/proces/exec_fork3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(void)
+{
+    pid_t parent_pid = getpid();
+
+    pid_t child_pid;
+    child_pid = fork();
+
+    if(child_pid < 0)
+    {
+        fprintf(stderr, "fork error\r\n");
+        exit(1);
+
+    } else if(child_pid == 0) {
+        pid_t child_pid = getpid();
+
+        printf("C%d: I'm children!\r\n", child_pid);
+        printf("C%d: exe new code\r\n", child_pid);
+
+        char* args[] = {"echo", "Hello!", (char*)NULL};
+
+        if(execvp("echo", args) == -1)
+        {
+            fprintf(stderr, "ecexlp error\r\n");
+        }
+
+        exit(1);
+
+    } else if(child_pid > 0) {
+
+        printf("P%d: I'm the parent!\r\n", parent_pid);
+        printf("P%d: wait to join with children\r\n", parent_pid);
+        while(wait(NULL)!=child_pid);
+        printf("P%d: joined with child. Exit.\r\n", parent_pid);
+        exit(0);
+    }
+
+    return 0;
+}
