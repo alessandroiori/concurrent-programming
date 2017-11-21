44c5eb840c6c63e053ca7239cb32b2e2320be080
alessandroiori <alessandro.iori92@gmail.com>
2017-11-22 00:08:57 +0100

    added first test on put_non_bloccante

---
+++ hcw1/functions.c
@@ -0,0 +1,54 @@
+//
+// Created by Alessandro Iori on 21/11/2017.
+//
+
+#include "prod_cons.h"
+
+pthread_mutex_t MUTEX;
+pthread_cond_t COND_NOT_FULL;
+pthread_cond_t COND_NOT_EMPTY;
+
+/* operazioni sul buffer */
+// inserimento bloccante: sospende se pieno, quindi
+// effettua l’inserimento non appena si libera dello spazio
+// restituisce il messaggio inserito; N.B.: msg!=null
+msg_t* put_bloccante(buffer_t* buffer, msg_t* msg)
+{
+    return BUFFER_ERROR;
+}
+
+// inserimento non bloccante: restituisce BUFFER_ERROR se pieno,
+// altrimenti effettua l’inserimento e restituisce il messaggio
+// inserito; N.B.: msg!=null
+msg_t* put_non_bloccante(buffer_t* buffer, msg_t* msg)
+{
+    msg_t* return_msg = BUFFER_ERROR;
+
+    pthread_mutex_lock(&MUTEX);
+    if(*buffer->p_size < *buffer->p_max_size)
+    {
+        buffer->msgs[*buffer->p_d] = msg;
+        *buffer->p_size = *buffer->p_size + 1;
+        *buffer->p_d = *buffer->p_d + 1;
+
+        return_msg = msg;
+        //pthread_cond_wait(&COND_NOT_FULL,&MUTEX_BUFFER);
+    }
+    pthread_mutex_unlock(&MUTEX);
+
+    return return_msg;
+}
+
+// estrazione bloccante: sospende se vuoto, quindi
+// restituisce il valore estratto non appena disponibile
+msg_t* get_bloccante(buffer_t* buffer)
+{
+    return BUFFER_ERROR;
+}
+
+// estrazione non bloccante: restituisce BUFFER_ERROR se vuoto
+// ed il valore estratto in caso contrario
+msg_t* get_non_bloccante(buffer_t* buffer)
+{
+    return BUFFER_ERROR;
+}
---
+++ hcw1/functions.h
@@ -0,0 +1,28 @@
+//
+// Created by Alessandro Iori on 21/11/2017.
+//
+
+#ifndef UNTITLED_PROD_CONS_H
+#define UNTITLED_PROD_CONS_H
+
+#include <pthread.h>
+#include "msg.h"
+#include "buffer.h"
+
+/* operazioni sul buffer */
+// inserimento bloccante: sospende se pieno, quindi
+// effettua l’inserimento non appena si libera dello spazio
+// restituisce il messaggio inserito; N.B.: msg!=null
+msg_t* put_bloccante(buffer_t* buffer, msg_t* msg);
+// inserimento non bloccante: restituisce BUFFER_ERROR se pieno,
+// altrimenti effettua l’inserimento e restituisce il messaggio
+// inserito; N.B.: msg!=null
+msg_t* put_non_bloccante(buffer_t* buffer, msg_t* msg);
+// estrazione bloccante: sospende se vuoto, quindi
+// restituisce il valore estratto non appena disponibile
+msg_t* get_bloccante(buffer_t* buffer);
+// estrazione non bloccante: restituisce BUFFER_ERROR se vuoto
+// ed il valore estratto in caso contrario
+msg_t* get_non_bloccante(buffer_t* buffer);
+
+#endif //UNTITLED_PROD_CONS_H
