6ab3265cbb9be334b0146a79449b49c84d55faea
alessandroiori <alessandro.iori92@gmail.com>
2017-12-16 18:54:54 +0100

    added concurrent reader with fake dispatcher

--- hwc2/test/test_reader.c
+++ hwc2/test/test_reader.c
@@ -4,6 +4,45 @@
 
 #include "test_reader.h"
 
+pthread_t TMP_DISPATCHER;
+buffer_concurrent_t* TMP_C_BUFFER;
+msg_t* TMP_MSGS;
+int TMP_MSGS_LEN;
+
+/* support */
+void* test_support_reader_fake_dispatcher_function(void* args)
+{
+    int i;
+    for(i=0; i < TMP_MSGS_LEN; i++)
+    {
+        buffer_concurrent_add_msg(TMP_C_BUFFER, &TMP_MSGS[i]);
+    }
+    buffer_concurrent_add_msg(TMP_C_BUFFER, POISON_PILL);
+    return (void*) NULL;
+}
+
+void test_support_reader_fake_dispatcher(buffer_concurrent_t* reader_c_buffer, msg_t* ms, int ms_len)
+{
+    TMP_C_BUFFER = reader_c_buffer;
+    TMP_MSGS = ms;
+    TMP_MSGS_LEN = ms_len;
+
+    if(pthread_create(&TMP_DISPATCHER, NULL, test_support_reader_fake_dispatcher_function, NULL))
+    {
+        printf("error creating dispatcher thread\t\n");
+        exit(1);
+    }
+}
+
+void test_support_reader_join_fake_dispatcher(void)
+{
+    if(pthread_join(TMP_DISPATCHER, NULL))
+    {
+        printf("error joining dispatcher thread\t\n");
+        exit(1);
+    }
+}
+
 /* Test */
 int reader_before(void)
 {
@@ -91,3 +130,23 @@ void test_reader_4_msg_letti_buffer_dim_5(void)
     reader->reader_destroy(reader);
     m->msg_destroy(m);
 }
+
+void test_reader_6_msg_letti_buffer_dim_5(void)
+{
+    char content[] = "content";
+    msg_t* m = msg_init_string(content);
+    msg_t ms[] = {*m, *m, *m, *m, *m, *m};
+    reader_t* reader = reader_init(1);
+
+    reader_start_thread(reader);
+    test_support_reader_fake_dispatcher(reader->c_buffer, ms, 6);
+    sleep(7);
+    test_support_reader_join_fake_dispatcher();
+
+    CU_ASSERT(0 == *reader->c_buffer->buffer->p_size);
+    CU_ASSERT(0 == strcmp(READER_LAST_MSG->content, POISON_PILL->content));
+
+    reader->reader_destroy(reader);
+    m->msg_destroy(m);
+}
+
--- hwc2/test/test_reader.h
+++ hwc2/test/test_reader.h
@@ -18,5 +18,11 @@ void test_reader_destroy(void);
 void test_reader_0_msg_letti_buffer_dim_5(void);
 void test_reader_1_msg_letto_buffer_dim_5(void);
 void test_reader_4_msg_letti_buffer_dim_5(void);
+void test_reader_6_msg_letti_buffer_dim_5(void);
+
+/* support */
+void* test_support_reader_fake_dispatcher_function(void* args);
+void test_support_reader_fake_dispatcher(buffer_concurrent_t* reader_c_buffer, msg_t* ms, int ms_len);
+void test_support_reader_join_fake_dispatcher(void);
 
 #endif //UNTITLED_TEST_READER_H
