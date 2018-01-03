9f95660e66b1e4e3ba8a5f460adcd95bba6d93c4
alessandroiori <alessandro.iori92@gmail.com>
2018-01-03 19:45:15 +0100

    refactor: removed some comments

--- hwj/hwj1/src/adder/Hwj1.java
+++ hwj/hwj1/src/adder/Hwj1.java
@@ -12,12 +12,39 @@ import java.util.Queue;
 import java.util.concurrent.*;
 
 public class Hwj1 extends BinaryTreeAdderThreads {
-        //implements BinaryTreeAdder {
-    //private static int nThreads;
     private static Semaphore semaphore;
 
     public Hwj1(String name) {
         super(name);
+    }
+
+    @Override
+    public int computeOnerousSum(Node root) {
+        int result = 0;
+        semaphore = new Semaphore(0);
+        List<Future<Integer>> penging = new LinkedList<>();
+        ConcurrentLinkedQueue<Node> buffer = new ConcurrentLinkedQueue<>(); //https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ConcurrentLinkedQueue.html
+        ExecutorService es = Executors.newFixedThreadPool(this.getNThreads());
+
+        if(root == null) { return 0; }
+
+        if(buffer.add(root)) { semaphore.release(1); }
+
+        for(int i=0; i<this.getNThreads(); i++) { penging.add(es.submit(new Task(buffer))); }
+
+        for(Future<Integer> f : penging) {
+            try { result += f.get(); }
+            catch (InterruptedException | ExecutionException ie) {}
+        }
+
+        es.shutdown();
+
+        return result;
+    }
+
+    //TODO: remove
+    public static void main(String args[]) {
+        new PerformanceCalculator().speedUpComputation(new Hwj1("HWJ1"), 15);
     }
 
     public static class Task implements Callable<Integer> {
@@ -62,75 +89,5 @@ public class Hwj1 extends BinaryTreeAdderThreads {
             //System.out.println("Thread " + Thread.currentThread().getName() + " exit, partial result: " + result);
             return result;
         }
-    }
-
-    @Override
-    public int computeOnerousSum(Node root) {
-        int result = 0;
-        semaphore = new Semaphore(0);
-        //nThreads = nThreads == 0 ? 1 : nThreads;
-        List<Future<Integer>> penging = new LinkedList<>();
-        ConcurrentLinkedQueue<Node> buffer = new ConcurrentLinkedQueue<>(); //https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/ConcurrentLinkedQueue.html
-        ExecutorService es = Executors.newFixedThreadPool(this.getNThreads());
-
-        if(root == null) { return 0; }
-
-        if(buffer.add(root)) { semaphore.release(1); }
-
-        for(int i=0; i<this.getNThreads(); i++) { penging.add(es.submit(new Task(buffer))); }
-
-        for(Future<Integer> f : penging) {
-            try { result += f.get(); }
-            catch (InterruptedException | ExecutionException ie) {}
-        }
-
-        es.shutdown();
-
-        return result;
-    }
-
-    /*public int computeOnerousSumSeriale(Node root) {
-        nThreads = 1;
-        return computeOnerousSum(root);
-
-    }
-
-    public int computeOnerousSumConcorrente(Node root) {
-        nThreads = Runtime.getRuntime().availableProcessors();
-        return computeOnerousSum(root);
-    }
-
-    public void setThreadsNumber(int n) {
-        nThreads = n;
-    }*/
-
-    public static void main(String args[]) {
-        /*
-        int depth = 3;
-        Hwj1 tmp = new Hwj1("HWJ1");
-        TreeNode rootNode = (TreeNode) new Tree().generateBinaryTree(depth);
-
-        PerformanceCalculator serialePc = new PerformanceCalculator();
-        tmp.setNThreads(1);
-        serialePc.startTime();
-        int serialeResult = tmp.computeOnerousSum(rootNode); // (2^(depth+1))-1
-        serialePc.stopTime();
-        long serialeElapsedTime = serialePc.getElapsedTime();
-
-        PerformanceCalculator concorrentePc = new PerformanceCalculator();
-        tmp.setNThreads(NCPU);
-        concorrentePc.startTime();
-        int concorrenteResult = tmp.computeOnerousSum(rootNode); //(2^(depth+1))-1
-        concorrentePc.stopTime();
-        long concorrenteElapsedTime = concorrentePc.getElapsedTime();
-
-        long speedup = new PerformanceCalculator().getSpeedUp(serialeElapsedTime, concorrenteElapsedTime);
-
-        System.out.print("[HWJ1] tree depth " + depth +
-                ", seriale (1 Thread) " + serialeElapsedTime / 1000000000.0 +
-                "s, concorrente ("+ NCPU +" Thread) " + concorrenteElapsedTime / 1000000000.0 +
-                "s, speedup: " + speedup);
-    */
-        new PerformanceCalculator().speedUpComputation(new Hwj1("HW1"), 10);
     }
 }
