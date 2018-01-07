68ffe692dd50623602945295302c818fab83566e
alessandroiori <alessandro.iori92@gmail.com>
2018-01-07 14:46:59 +0100

    renamed hwj_new in hwj

--- hwj/src/HWJ.java
+++ hwj/src/HWJ.java
@@ -2,7 +2,6 @@ import adder.Hwj1;
 import adder.Hwj3;
 import adder.Hwj4;
 import adder.SerialBinaryTreeAdder;
-
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import tree.Tree;
@@ -11,6 +10,7 @@ import utils.PerformanceCalculator;
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 import static adder.SerialBinaryTreeAdder.NCPU;
 
@@ -21,8 +21,8 @@ public class HWJ {
         DecimalFormat df = new DecimalFormat("#.##");
         df.setRoundingMode(RoundingMode.CEILING);
 
-        System.out.println("[INFO] max threads number " + NCPU);
-        for(int treeDepth = 10; treeDepth <= 18; treeDepth+=2) {
+        System.out.println("[SU_INFO] max threads number " + NCPU);
+        for(int treeDepth = 2; treeDepth <= 18; treeDepth+=2) {
             TreeNode rootTree = (TreeNode) new Tree().generateBinaryTree(treeDepth);
 
             pc.clean();
@@ -55,8 +55,26 @@ public class HWJ {
 
     public static void main(String args[]) {
         JUnitCore junit = new JUnitCore();
-        Result result = junit.run(test.HwjSuitTest.class);
-        System.out.println(result.wasSuccessful());
-        //new HWJ().speedUpTest();
+        ArrayList<Result> results = new ArrayList<>();
+        PerformanceCalculator pc = new PerformanceCalculator();
+
+        pc.startTime();
+        //results.add(junit.run(test.TreeNodeTest.class));
+        //results.add(junit.run(test.TreeTest.class));
+        //results.add(junit.run(test.SpliteratorTreeTest.class));
+        //results.add(junit.run(test.SerialBinaryTreeAdderTest.class));
+        results.add(junit.run(test.Hwj1Test.class));
+        results.add(junit.run(test.Hwj3Test.class));
+        results.add(junit.run(test.Hwj4Test.class));
+
+        results.stream().forEach(r -> {
+            if(!r.wasSuccessful()) System.out.println(r.getFailures());
+            return;
+        });
+
+        new HWJ().speedUpTest();
+        pc.stopTime();
+
+        System.out.println("Elapsed execution time: " + pc.getElapsedTime() / 1000000000.0 + "s");
     }
 }
